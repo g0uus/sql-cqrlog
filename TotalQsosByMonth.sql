867ded883e5036b4bb18e6998e617130fed1f08e
Graham <graham.g0uus@gmail.com>
2025-10-10 15:53:07 +0100

    missed from last comit :(

--- TotalQsosByMonth.sql
+++ TotalQsosByMonth.sql
@@ -2,4 +2,7 @@ SELECT DATE_FORMAT(qsodate, '%Y-%m') AS month, COUNT(*) AS qso_count
 FROM cqrlog_main
 GROUP BY
     month
-ORDER BY month;
+ORDER BY month -- Stop INTO clause being put onto one line !
+    INTO OUTFILE '~/QsoCountByMonth.csv'
+    -- stop
+    FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
