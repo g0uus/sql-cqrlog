8ff3d107e6b46afc3e97201476f5240d186637d9
Graham Hanson <graham.g0uus@gmail.com>
2025-11-28 13:32:41 +0000

    Added count of unique grid squares worked.

---
+++ CountUnique4CharGridsquares.sql
@@ -0,0 +1,6 @@
+-- Count unique 4-character gridsquares from QSO locations (loc field)
+-- Extracts the first 4 characters from the locator field
+
+SELECT COUNT(DISTINCT SUBSTRING(loc, 1, 4)) AS unique_4char_gridsquares
+FROM view_cqrlog_main_by_qsodate
+WHERE loc IS NOT NULL AND loc != '';
