7f28fc85bfaa1138e18048c29a0f03f3b32227e9
Graham Hanson <graham.g0uus@gmail.com>
2025-12-19 18:06:18 +0000

    Updated to generat YYYY-MM-01 to allow scatter chart usage

--- CumulativeQsosByMonth.sql
+++ CumulativeQsosByMonth.sql
@@ -1,17 +1,22 @@
+--
+-- NOTE the output file is written to a path on the database server host
+-- not the client host.
+-- Also CANNOT OVERWRITE AND EXISTING FILE!
+--
 SELECT
     month,
     qso_count,
     SUM(qso_count) OVER (
-        ORDER BY month
+        ORDER BY month  
     ) AS cumulative_qso_count
 FROM (
-        SELECT DATE_FORMAT(qsodate, '%Y-%m') AS month, COUNT(*) AS qso_count
+        SELECT DATE_FORMAT(qsodate, '%Y-%m-01') AS month, COUNT(*) AS qso_count
         FROM cqrlog_main
         GROUP BY
             month
     ) AS monthly_counts
 ORDER BY month
     -- Stop INTO clause being put onto one line !
-    INTO OUTFILE '~/CumulativeQsosByMonth.csv'
+    INTO OUTFILE '/home/graham/CumulativeQsosByMonth.csv'
     -- stop
     FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
