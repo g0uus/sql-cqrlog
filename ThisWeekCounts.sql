f738399c71e50a78a3950b8082634bb7a758ece3
Graham Hanson <graham.g0uus@gmail.com>
2026-01-15 16:40:59 +0000

    Added This Week Counts query

---
+++ ThisWeekCounts.sql
@@ -0,0 +1,50 @@
+--
+--
+--
+
+select
+    case
+        when qsodate = CURRENT_DATE then 'Today'
+        when qsodate = CURRENT_DATE - INTERVAL 1 DAY then 'Yesterday'
+        else to_char (qsodate, 'Day')
+    end as day_label,
+    sum(
+        case
+            when mode = 'SSB' then 1
+            else 0
+        end
+    ) as ssb,
+    sum(
+        case
+            when mode = 'CW' then 1
+            else 0
+        end
+    ) as cw,
+    sum(
+        case
+            when mode = 'FT4' then 1
+            else 0
+        end
+    ) as ft4,
+    sum(
+        case
+            when mode = 'FT8' then 1
+            else 0
+        end
+    ) as ft8,
+    sum(
+        case
+            when mode <> "SSB"
+            and mode <> "CW"
+            and mode <> "FT4"
+            and mode <> "FT8" then 1
+            else 0
+        end
+    ) as other,
+    count(*) as total
+from view_cqrlog_main_by_qsodate
+where
+    qsodate >= CURRENT_DATE - INTERVAL 6 DAY
+group by
+    qsodate
+order by qsodate desc;
